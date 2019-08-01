6f834081c55a0b093f1740a394c82b5269a675f5
DekiRostilj7 <dejan.rosulj89@gmail.com>
2019-08-01 19:15:56 +0200

    zavrsen 6. zadatak (stavka 13)

--- comments.php
+++ comments.php
@@ -1,6 +1,8 @@
 <?php
 
 
+
+
         $sqlComments =
         "SELECT * FROM comments WHERE comments.Post_id = {$_GET['post_id']}";
         "SELECT * FROM comments JOIN users ON comments.Post_id = posts.Id WHERE comments.Post_id = {$_GET['post_id']}";
@@ -15,18 +17,42 @@
 ?>
 <body>
         
-        <?php
-
-foreach ($comments as $comment) {
-        ?>
+        
             
         <div class = "comment-div">
             <ul id="hide">
+        <?php
+
+                foreach ($comments as $comment) {
+        ?>
+
+
                 <li  class="single-comment">
-                <div>Posted by: <?php echo $comment['Author'] ?></div>
-                <div> comment: <?php echo $comment['Text'] ?> </div>
+                        <div>Posted by: <?php echo $comment['Author'] ?></div>
+                        <div> comment: <?php echo $comment['Text'] ?> </div>
                 </li>
-<?php } ?>
+       
+
+       
+        <?php } ?>
+
+         </ul>
+
+        <button id= "button" onclick="hideComment()">Hide comments</button>
+        <script>
+        var comments = document.getElementById('hide')
+        var button = document.getElementById('button')
+        function hideComment(){
+                
+                if(button.innerHTML == "Show Comments"){
+                        comments.classList.remove("hide")
+                        button.innerHTML = "Hide Comments"
+                } else{
+                        comments.className = "hide"
+                        button.innerHTML = "Show Comments"
+                }
+        }
+</script>
 
        
 </body>
--- single-post.php
+++ single-post.php
@@ -1,4 +1,19 @@
 <html>
+<head>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="">
+<meta name="author" content="">
+<link rel="icon" href="../../../../favicon.ico">
+
+<title>Vivify Blog</title>
+
+<!-- Bootstrap core CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+
+<!-- Custom styles for this template -->
+<link href="styles/blog.css" rel="stylesheet">
     <body>
         <?php 
             include('db.php');
@@ -20,7 +35,7 @@
         ?>
 
 
-
+     <main role="main" class="container">       
                 
         <div class="blog-post">
                 <h2 class="blog-post-title"><a href="single-post.php?post_id=<?php echo($singlePost['Id']) ?>">
@@ -36,6 +51,6 @@
         <?php include('sidebar.php'); ?>
 
         <?php include('footer.php'); ?>
-        
+        </main>
     </body>
 </html>
