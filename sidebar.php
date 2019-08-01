c8c3ac73eff048b918c4444396eb361f8f23d369
DekiRostilj7 <dejan.rosulj89@gmail.com>
2019-08-01 19:33:12 +0200

    zavrsen 7. zadatak

--- sidebar.php
+++ sidebar.php
@@ -1,11 +1,22 @@
+<?php 
+    include('db.php');
 
+$sql = "SELECT * FROM posts ORDER BY Created_at desc LIMIT 5";
+$statement = $connection->prepare($sql);
+$statement->execute();
 
+$statement->setFetchMode(PDO::FETCH_ASSOC);
+    $posts = $statement->fetchAll();
 
+?>
 
 <aside class="col-sm-3 ml-sm-auto blog-sidebar">
+    <?php 
+        foreach($posts as $post ) { ?>
             <div class="sidebar-module sidebar-module-inset">
-                <h4 class="side-title">Latest posts</h4>
-                <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+                <h4 class="side-title"><a href="single-post.php?post_id=<?php echo($post['Id']) ?>">
+                    <?php echo $post['Title']; ?>
+                </a></h4>
             </div>
-           
+        <?php } ?>
 </aside>
